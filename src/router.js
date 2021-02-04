b371880e78d141e1a1351f48ff553b5c3343ad9f
知晓同丶 <947803089@qq.com>
2021-02-04 15:58:54 +0800

    fix：undo router mode

--- src/router.js
+++ src/router.js
@@ -1,16 +1,15 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Index from "./views/index.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
+import Index from './views/index.vue'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
   routes: [
     {
-      path: "/",
-      name: "index",
+      path: '/',
+      name: 'index',
       component: Index
     }
   ]
-});
+})
