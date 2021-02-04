0f9799dd823bb3d0de876f51fab4d4f76dde3fef
知晓同丶 <947803089@qq.com>
2021-02-04 15:05:17 +0800

    build：update dependent resources

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    endOfLine: 'auto',
+    semi: false,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'none'
+}
